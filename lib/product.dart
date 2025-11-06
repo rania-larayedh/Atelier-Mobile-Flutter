ac14ce36c3a866175dc7f6425bf72d933121beda
Rania Larayedh <ranialarayedh9@gmail.com>
2025-11-06 12:16:19 +0100

    modification atelier 3

---
+++ lib/product.dart
@@ -0,0 +1,16 @@
+class Product {
+  final String name;
+  final double price;
+  final double rating;
+  final String image; 
+
+  Product({
+    required this.name,
+    required this.price,
+    required this.rating,
+    required this.image,
+  });
+}
+
+
+
